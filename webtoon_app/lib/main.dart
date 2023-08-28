c807877a29c4f40cf85c1a0011818174ea3dc144
김민선 <alstjs4739@kookmin.ac.kr>
2023-08-28 13:48:02 +0900

    feat. 홈 화면 상단바 구현

---
+++ webtoon_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon_app/home_screen.dart';
+
+void main() {
+  runApp(const WebtoonApp());
+}
+
+class WebtoonApp extends StatelessWidget {
+  const WebtoonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
