dd0efebf39eb80fb6c0fe7143e176d1c4fe34b38
김민선 <alstjs4739@kookmin.ac.kr>
2023-08-31 12:51:25 +0900

    feat. 백엔드 연동 데이터 변수 저장 기능 구현

--- webtoon_app/lib/services/api_service.dart
+++ webtoon_app/lib/services/api_service.dart
@@ -7,15 +7,16 @@ class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      return;
+      return webtoonInstances;
     }
     throw Error();
   }
